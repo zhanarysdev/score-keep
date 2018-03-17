4b23494c163beb36b56257792269e3caf8ef411d
Zhanarys Mataev <mzhanarys@pridesytems.com>
2018-03-18 00:50:07 +0600

    added react and mongoDB

--- client/main.js
+++ client/main.js
@@ -1,22 +1,48 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker' 
+import { Players } from './../imports/api/players'
 
-import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+const renderPlayesrs = (playersList) => {
+  return playersList.map((player) => {
+    return <p key={player._id} >{player.name} has {player.score} point(s)  </p>
+  })
+}
+const handeSubmit = (e) => {
+  const playerName = e.target.playerName
+  e.preventDefault()
+ 
+    Players.insert({
+      name: playerName,
+      score: 0
+    })
+  
+}
+
+Meteor.startup(()=>{
+  Tracker.autorun(()=>{
+   const players =  Players.find().fetch()
+  
+  
+  let title = 'Score keep'
+  let name = 'mike'
+  let jsx = (
+    <div>
+      <h1>{title}</h1>
+      <p>Hello {name}</p>
+      <p>This is my second p</p>
+      {renderPlayesrs(players)}
+      <form onSubmit={handeSubmit}>
+        <input type="text" name="playerName" placeholder="Player name" />
+        <button>Add player</button>
+      </form>
+    </div>
+  )
+  ReactDOM.render(jsx, document.getElementById('root'))
+})
+
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor'
+import { Players } from './../imports/api/players'
+
 
 Meteor.startup(() => {
-  // code to run on server at startup
-});
+
+  
+})
